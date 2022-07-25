ae76ec6cead62e5509150cdef8e0f4d11b27730d
dueityou <dueityou@gmail.com>
2022-07-25 15:19:26 +0900

    serviceContainer Test

---
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LifeCycleTestController extends Controller
+{
+    public function showServiceContainerTest() {
+        //サービスコンテナの登録
+        app()->bind('lifeCycleTest', function() {
+            return 'ライフサイクルテスト';
+        });
+
+        //サービスコンテナの呼び出し
+        $test = app()->make('lifeCycleTest');
+
+        //サービスコンテナなしのパターン
+        //まずそれぞれのクラスをインスタンス化して注入する必要がある。
+        // $message = new Message();
+        // $sample = new Sample($message);
+        // $sample->run();
+
+
+        //サービスコンテナapp()ありのパターン
+        //Sampleクラスを結びつける
+        app()->bind('sample', Sample::class);
+        //Sampleクラスの呼び出し
+        $sample = app()->make('sample');
+        //sampleクラス内のrunメソッドを実行
+        $sample->run();
+
+        dd($test, app());
+    }
+}
+
+class Sample {
+    public $message;
+    //DI（依存性の注入） => 引数にクラスを入れることで自動的にインスタンス化をしてくれる。
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+    public function run() {
+        //Messageクラスで定義したsendメソッドを使用できる。
+        $this->message->send();
+    }
+}
+
+class Message {
+    public function send() {
+        echo('メッセージ表示');
+    }
+}
