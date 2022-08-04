bb735bb78e607f3a73f503ec302e952fa2081eab
dueityou <dueityou@gmail.com>
2022-08-04 14:44:04 +0900

    stock

---
+++ app/Models/Stock.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Stock extends Model
+{
+    use HasFactory;
+
+    //テーブル名の変更
+    protected $table = 't_stocks';
+}
