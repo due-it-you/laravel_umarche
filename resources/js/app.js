b28d7bcc55ee71412e4886bdddd711b85c2c2531
dueityou <dueityou@gmail.com>
2022-08-04 17:43:16 +0900

    product create

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,11 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import MicroModal from 'micromodal';
+MicroModal.init({
+    disableScroll : true
+});
+
 window.Alpine = Alpine;
 
 Alpine.start();
