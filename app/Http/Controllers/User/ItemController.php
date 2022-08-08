f33cadccf65eceb613096d3a39e469301a1c7193
dueityou <dueityou@gmail.com>
2022-08-08 15:26:45 +0900

    コンストラクタ

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -13,39 +13,28 @@ class ItemController extends Controller
     public function __construct()
     {
         $this->middleware('auth:users');
+
+        //停止中（is_selling = 0)の商品を表示しないようにする処理
+        $this->middleware(function ($request, $next) {
+
+            
+            $id = $request->route()->parameter('item');  //ルートパラメータの取得 
+            if(!is_null($id)) { //null判定（この場合は、”空じゃなったら”という分岐）
+                $itemId = Product::availableItems()->where('products.id', $id)->exists(); //現在販売中かつ在庫のある商品だけを取ってきて、trueかfalseかの判定を返す。
+
+                if(!$itemId) { //もしtrueじゃなければ
+                    abort(404); //404画面を出力
+                }
+            }
+           return $next($request); 
+        });
     }
 
 
     public function index() {
-        //クエリビルダでの商品情報の取得処理
 
-        //product_idを纏めた上で、合計在庫総数を取ってくる。(ただし在庫数は1以上のものに限る。)
-        $stocks = DB::table('t_stocks')
-        ->select('product_id',
-        DB::raw('sum(quantity) as quantity'))
-        ->groupBy('product_id')
-        ->having('quantity', '>', 1);
-
-        $products = DB::table('products')
-            ->joinSub($stocks, 'stock', function($join){
-                $join->on('products.id', '=', 'stock.product_id');
-            })
-            //products, shops, secondary_categories, image1~4のそれぞれのテーブルとカラムを連結させる。
-            ->join('shops', 'products.shop_id', '=', 'shops.id')
-            ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
-            ->join('images as image1', 'products.image1', '=', 'image1.id')
-            ->join('images as image2', 'products.image2', '=', 'image2.id')
-            ->join('images as image3', 'products.image3', '=', 'image3.id')
-            ->join('images as image4', 'products.image4', '=', 'image4.id')
-
-            //"販売中"のもののみを取ってくる。
-            ->where('shops.is_selling', true)
-            ->where('products.is_selling', true)
-            //商品ID、商品名、価格、表示順番、商品情報、小カテゴリー、第一画像のファイル名　を取得 (今回はEloquantで取得するのではなく、クエリビルダで取得するためselectを使う)
-            ->select('products.id as id', 'products.name as name', 'products.price', 'products.sort_order as sort_order'
-                        ,'products.information', 'secondary_categories.name as category', 'image1.filename as filename')
-            ->get();
-
+        //全ての商品を取得する処理（ローカルスコープでまとめている）
+        $products = Product::availableItems()->get();
 
         return view('user.index', compact('products'));
     }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Shop;
 use App\Models\SecondaryCategory;
@@ -59,5 +60,30 @@ class Product extends Model
     {
         return $this->belongsToMany(User::class, 'carts')
         ->withPivot(['id','quantity']);
+    }
+
+    public function scopeAvailableItems($query)
+    {
+        //クエリビルダでの商品情報の取得処理
+
+        //product_idを纏めた上で、合計在庫総数を取ってくる。(ただし在庫数は1以上のものに限る。)
+        $stocks = DB::table('t_stocks')
+        ->select('product_id',
+        DB::raw('sum(quantity) as quantity'))
+        ->groupBy('product_id')
+        ->having('quantity', '>', 1);
+
+        return $query->
+        //全てのテーブルを連結
+        //products, shops, secondary_categories, image1のそれぞれのテーブルとカラムを連結させる。
+          join('shops', 'products.shop_id', '=', 'shops.id')
+        ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
+        ->join('images as image1', 'products.image1', '=', 'image1.id')
+        //"販売中"のもののみを取ってくる。
+        ->where('shops.is_selling', true)
+        ->where('products.is_selling', true)
+        //商品ID、商品名、価格、表示順番、商品情報、小カテゴリー、第一画像のファイル名　を取得 (今回はEloquantで取得するのではなく、クエリビルダで取得するためselectを使う)
+        ->select('products.id as id', 'products.name as name', 'products.price', 'products.sort_order as sort_order'
+                    ,'products.information', 'secondary_categories.name as category', 'image1.filename as filename');
     }
 }
